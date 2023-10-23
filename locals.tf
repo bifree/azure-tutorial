8a008329f819126d179c8993e013412cc7464e8a
bifree <bifree2bfree@gmail.com>
2023-10-23 21:21:41 +0900

    Start azure learning

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  name                      = "udemy-resource-group"
+  location                  = "japaneast"
+  vnet_name                 = "udemy-virtual-network"
+  public_subnet_name        = "public"
+  private_subnet_name       = "private"
+  vm_network_interface_name = "udemy-network-interface"
+}
